0a29a41c997adbe7136c95dd5fce9ccc09982393
Morgan VanYperen <morgvanny@gmail.com>
2020-07-29 10:25:56 -0700

    mvc+crud

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  get '/books' do
+    @books = Book.all
+    erb :'books/index'
+  end
+
+  get '/books/new' do
+    @book = Book.new
+    erb :'books/new'
+  end
+
+  get '/books/:id' do
+    @book = Book.find(params[:id])
+    erb :'books/show'
+  end
+
+  get '/books/:id/edit' do
+    @book = Book.find(params[:id])
+    erb :'books/edit'
+  end
+
+  post '/books' do
+    @book = Book.new(params)
+    if @book.save
+      redirect to "/books/#{@book.id}"
+    else
+      @error = @book.errors.full_messages.first
+      erb :'books/new'
+    end
+  end
+
+  patch '/books/:id' do
+    @book = Book.find(params[:id])
+    params.delete(:_method)
+     #mass assignment can be dangerous if you don't sanitize params
+    if @book.update(params)
+      redirect to "/books/#{@book.id}"
+    else
+      @error = @book.errors.full_messages.first
+      erb :'books/edit'
+    end
+  end
+
+  delete '/books/:id' do
+
+  end
+end
