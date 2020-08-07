9d12863c56fdada099cb72cd122294dfd66be6f6
Morgan VanYperen <morgvanny@gmail.com>
2020-08-07 11:54:13 -0700

    ready to deploy now

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,13 @@ Bundler.require
 # require 'sinatra/cookies'
 require_all 'app'
 
-ActiveRecord::Base.establish_connection({
-  adapter: 'postgresql',
-  database: ENV['DATABASE_URL'] || 'postgres://localhost/mydb'
-})
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
