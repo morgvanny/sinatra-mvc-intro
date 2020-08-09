9db1a0522907b74af6daba92997e140ed3cef540
Morgan VanYperen <morgvanny@gmail.com>
2020-08-08 21:46:57 -0700

    database setup

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'bundler'
 Bundler.require
 require_all 'app'
 
-ActiveRecord::Base.establish_connection({
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || {
   adapter: 'sqlite3',
   database: 'db/development.sqlite'
 })
